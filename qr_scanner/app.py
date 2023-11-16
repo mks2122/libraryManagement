182d28d10758f8deeb1e14b546371c6d844ea56d
Karthick Sriram <karthicksriram71@gmail.com>
2023-11-16 19:26:45 +0530

    added box in scanner

--- qr_scanner/app.py
+++ qr_scanner/app.py
@@ -4,7 +4,7 @@ from pyzbar.pyzbar import decode
 import numpy as np
 from PIL import Image
 
-def qr_barcode_scanner(image):
+def qr_barcode_scanner(image, box_coords=None):
     # Convert PIL image to numpy array
     image_data = np.array(image)
 
@@ -12,8 +12,13 @@ def qr_barcode_scanner(image):
     if len(image_data.shape) == 3 and image_data.shape[2] == 3:
         image_data = cv2.cvtColor(image_data, cv2.COLOR_RGB2GRAY)
 
-    # Decode QR codes and barcodes
-    decoded_objects = decode(image_data)
+    # Define coordinates for the box
+    if box_coords is not None:
+        x, y, w, h = box_coords
+        roi = image_data[y:y+h, x:x+w]  # Region of interest within the box
+        decoded_objects = decode(roi)
+    else:
+        decoded_objects = decode(image_data)
 
     if len(decoded_objects) == 0:
         return "No QR code or barcode found."
@@ -42,16 +47,25 @@ while True:
         print("Error: Failed to capture frame.")
         break
 
+    # Define the box coordinates (x, y, width, height)
+    box_x, box_y, box_w, box_h = 100, 100, 500, 200  # Adjust these values as needed
+
+    # Draw the box on the frame
+    cv2.rectangle(frame, (box_x, box_y), (box_x + box_w, box_y + box_h), (0, 0, 0), 2)
+
+    cv2.putText(frame, "Place the barcode inside the box", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
     # Display the frame
     cv2.imshow("Camera Feed", frame)
 
     # Break the loop if the 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    a=qr_barcode_scanner(frame)
-    # print(qr_barcode_scanner(frame))
 
-    if a!="No QR code or barcode found.":
+    # Perform QR code/barcode scanning only within the defined box
+    a = qr_barcode_scanner(frame, box_coords=(box_x, box_y, box_w, box_h))
+
+
+    if a != "No QR code or barcode found.":
         print(a)
         break
     else:
