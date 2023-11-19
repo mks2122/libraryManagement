4c7ab8e3a6511520e52ec6b5f4d358054602a369
Kausik D <kausikdevanathan@gmail.com>
2023-11-19 18:21:49 +0530

    Added Database operations

--- DBOps/database_operations.py
+++ DBOps/database_operations.py
@@ -74,6 +74,90 @@ def return_book(book_id,user_id):
 
 
 
-return_book("0060887966",'22AM112')
+# return_book("0060887966",'22AM112')
 
+def fetch_all_books():
+    """
+    This function retuns a list of all books available in the library.
+    This is useful for the admin view the books in the library.
+    :return: list of all books
+    """
+    global books
+    connection = sql.connect(
+        host='localhost',
+        user='root',
+        password='Kausik@2204',
+        db='library'
+    )
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT * FROM books WHERE lent=0")
+        books = cursor.fetchall()
+    except Exception as e:
+        print(e)
+    finally:
+        return books
+def fetch_availablity_of_book(identifier, by_name=True):
+    """
+    This function returns the availability of a book in the library.
+    :param identifier: Book name or book ID based on the search type.
+    :param by_name: If True, search by book name; if False, search by book ID.
+    :return: Book information if found, None otherwise.
+    """
+    connection = sql.connect(
+        host='localhost',
+        user='root',
+        password='Kausik@2204',
+        db='library'
+    )
+    cursor = connection.cursor()
 
+    try:
+        if by_name:
+            cursor.execute(f"SELECT * FROM books WHERE bname='{identifier}'")
+        else:
+            cursor.execute(f"SELECT * FROM books WHERE id='{identifier}'")
+
+        book = cursor.fetchone()
+        return book
+
+    except Exception as e:
+        print(e)
+    finally:
+        connection.close()
+def fetch_lent_books():
+    """
+    This function returns a list of all the lent books with additional details such as
+    the user information, date lent, date of return, and number of reminders.
+    :return: list of lent books with details
+    """
+    connection = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='Kausik@2204',
+        db='library'
+    )
+    cursor = connection.cursor(pymysql.cursors.DictCursor)  # Using DictCursor for a dictionary-like result
+
+    try:
+        # Using INNER JOIN to combine information from the 'books', 'user', and 'drem' tables
+        query = """
+        SELECT books.id AS book_id, books.bname AS book_name, books.genre,
+                   user.uid AS user_id, user.uname AS user_name,
+                   drem.rented AS date_lent, drem.due AS date_of_return, drem.ext AS reminders_sent
+            FROM books
+            INNER JOIN user ON books.id = user.bookid
+            INNER JOIN drem ON books.id = drem.bkid
+            WHERE books.lent = 1"""
+        cursor.execute(query)
+        lent_books_details = cursor.fetchall()
+
+        return lent_books_details
+
+    except Exception as e:
+        print(e)
+    finally:
+        connection.close()
+
+print(fetch_availablity_of_book("The Prophet",by_name=True))
+lend_books("0060887966",'22AM112')
